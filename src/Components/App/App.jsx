447f9838c40b4312d7df4835f07341c873f9e5cf
lpolon <lpolon@gmail.com>
2020-03-15 17:18:58 -0300

    NavBar static behaviour and styling
    - NavBar is fixed top

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import 'bulma/css/bulma.css';
 import './App.css';
+
+import React from 'react';
+
+import NavBar from '../NavBar/NavBar';
 
 export default function App() {
   return (
-    <div className="App">
-      WIP ;)
+    <div className="App" >
+      <NavBar />
     </div>
   );
 }
