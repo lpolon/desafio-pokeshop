d23ac3b1ebe6ebc84e61ce41aee80ef10381ce96
lpolon <lpolon@gmail.com>
2020-03-16 15:09:15 -0300

    App.css viewport height adjustment
    added 1vh to top property and removed 1vh from .App height to create some separation from navBar

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,14 +1,17 @@
 import 'bulma/css/bulma.css';
+import 'animate.css';
 import './App.css';
 
 import React from 'react';
 
 import NavBar from '../NavBar/NavBar';
+import SearchBar from '../SearchBar/SearchBar';
 
 export default function App() {
   return (
-    <div className="App" >
+    <div className="App">
       <NavBar />
+      <SearchBar />
     </div>
   );
 }
