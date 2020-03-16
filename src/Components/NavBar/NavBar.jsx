e743916bf6c177770587c79d5018ee882804ec28
lpolon <lpolon@gmail.com>
2020-03-15 22:18:33 -0300

    navbar is styled and responsive
    stateless behaviour

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,72 +1,56 @@
 import './NavBar.css';
-import React from 'react';
+
+import { FontAwesomeIcon as FA } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
+import React, { useState } from 'react';
 
 export default function NavBar() {
-  return ( 
-  <nav className="navbar is-fixed-top" style={{border: '1px solid black'}} role="navigation" aria-label="main navigation">
-    
-  </nav>);
-}
+  console.log('render navbar!');
+  const [toggle, set] = useState(false);
 
-/*
-<nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-    </a>
-
-    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-      <a class="navbar-item">
-        Home
-      </a>
-
-      <a class="navbar-item">
-        Documentation
-      </a>
-
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More
+  const handleToggle = () => set(!toggle)
+  return (
+    <nav
+      className="navbar is-fixed-top"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <a className="navbar-item">nome loja</a>
+        <a
+          onClick={handleToggle}
+          role="button"
+          className={`navbar-burger burger ${toggle ? 'is-active' : ''}`}
+          aria-label="menu"
+          aria-expanded="false"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
         </a>
+      </div>
+      <div className={`navbar-menu ${toggle ? 'is-active' : ''}`}>
+        <div className="navbar-start">
+          <div className="navbar-item has-dropdown is-hoverable">
+            <a className="navbar-link">Lojas</a>
+            <div className="navbar-dropdown">
+              <a className="navbar-item">Loja 1</a>
+              <a className="navbar-item">Loja 2</a>
+            </div>
+          </div>
+        </div>
 
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            About
-          </a>
-          <a class="navbar-item">
-            Jobs
-          </a>
-          <a class="navbar-item">
-            Contact
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            Report an issue
-          </a>
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <a className="button">
+              <span className="icon">
+                <FA icon={faShoppingCart} />
+              </span>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light">
-            Log in
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-</nav>
-*/
+    </nav>
+  );
+}
