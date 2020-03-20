d0ed485838d74949cb14c2ef8adc8366e0eeedf1
lpolon <lpolon@gmail.com>
2020-03-20 00:02:04 -0300

    fixed NavBar.jsx to remove warnings from react

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,21 +3,21 @@ import './NavBar.css';
 import { FontAwesomeIcon as FA } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function NavBar({ handleClick }) {
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
-        <a className="navbar-item">nome loja</a>
+        <span className="navbar-item">nome loja</span>
       </div>
       <div className="navbar-menu">
         <div className="navbar-start">
           <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link">Lojas</a>
+            <span className="navbar-link">Lojas</span>
             <div className="navbar-dropdown">
-              <a className="navbar-item">Loja 1</a>
-              <a className="navbar-item">Loja 2</a>
+              <span className="navbar-item">Loja 1</span>
+              <span className="navbar-item">Loja 2</span>
             </div>
           </div>
         </div>
@@ -26,11 +26,10 @@ export default function NavBar({ handleClick }) {
           <div className="navbar-item">
             <input
               onClick={handleClick}
-              className="button"
+              className="button switch"
               id="switchNormal"
               type="checkbox"
               name="switchNormal"
-              className="switch"
             />
             <label htmlFor="switchNormal">
               <FA icon={faShoppingCart} />
